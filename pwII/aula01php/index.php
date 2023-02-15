3568a4856f7a28af26c0757c5c0fb4146337acb2
PedroFAraujo <pedrofeearaujo@gmail.com>
2023-02-15 09:31:24 -0300

    ⠀

---
+++ pwII/aula01php/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo "Aula"; ?></title>
+</head>
+<body>
+    <?php 
+        echo "<h1>Hello, World!</h1>"; 
+        
+        $a = 1;
+        $b = 2;
+
+        echo "Multiplicação: " . $a * $b . "<br> Divisão: " . $a / $b . "<br> Subtração: " . $a - $b . "<br> Adição: " . $a + $b   ;
+
+    ?>
+
+</body>
+</html>
