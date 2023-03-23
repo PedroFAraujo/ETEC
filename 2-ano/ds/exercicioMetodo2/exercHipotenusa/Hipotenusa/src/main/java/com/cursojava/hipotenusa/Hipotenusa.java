1807ace62d9b07e7986bb40c4e1d85aab670e15d
Pedro Araújo <99838816+PedroFAraujo@users.noreply.github.com>
2023-03-23 13:51:44 -0300

    Add files via upload

---
+++ 2-ano/ds/exercicioMetodo2/exercHipotenusa/Hipotenusa/src/main/java/com/cursojava/hipotenusa/Hipotenusa.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.cursojava.hipotenusa;
+
+import javax.swing.JOptionPane;
+
+//@author Pedro
+ 
+public class Hipotenusa { //criacao classe hipotenusa
+    
+    static void hipotenusa(){ //criacao metodo hipotenusa
+        float base = Float.parseFloat(JOptionPane.showInputDialog("Digite a base da hipotenusa: ")); //criacao e atribuicao da variavel base 
+        float altura = Float.parseFloat(JOptionPane.showInputDialog("Digite a altura da hipotenusa: ")); //criacao e atribuicao da variavel altura
+        double hipotenusa = Math.sqrt(Math.pow(base,2) + Math.pow(altura,2)); //calculo da hipotenusa
+        String resultadoHipotenusa = String.format("%.2f",hipotenusa); //formatacao de casas decimais da hipotenusa
+        JOptionPane.showMessageDialog(null, "Hipotenusa = " + resultadoHipotenusa); //exibicao da hipotenusa
+    }
+    
+    public static void main(String[] args) { //metodo principal
+        hipotenusa(); //chama o metodo hipotenusa()
+    }
+}
