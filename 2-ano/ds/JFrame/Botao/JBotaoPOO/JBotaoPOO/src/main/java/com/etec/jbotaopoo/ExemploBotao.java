edc15be368ff2ad3835d04cb120153f93729ffac
PedroFAraujo <pedrofeearaujo@gmail.com>
2023-04-20 13:32:46 -0300

    ⠀

---
+++ 2-ano/ds/JFrame/Botao/JBotaoPOO/JBotaoPOO/src/main/java/com/etec/jbotaopoo/ExemploBotao.java
@@ -0,0 +1,45 @@
+package com.etec.jbotaopoo;
+
+//@author Pedro
+
+//importação das bibliotecas
+import javax.swing.*; 
+import java.awt.*;
+
+
+public class ExemploBotao extends JFrame{
+    JButton novoBtn, abrirBtn, botaoBtn; //criação das variáveis botao que recebe JButton
+    ImageIcon icone; //variavel ImageIcon para receber o icone
+    
+    //criação da função Jbotao para criar a interface gráfica
+    public ExemploBotao(){
+        super("Jbutton"); //nome da janela
+        Container tela = getContentPane(); //criação do container com o nome tela
+        setLayout(null); //limpar objetos da tela
+        
+        icone = new ImageIcon("img.gif");
+        
+        //atribuindo valor aos JButton
+        novoBtn = new JButton("Novo");
+        abrirBtn = new JButton("Abrir", icone);
+        botaoBtn = new JButton(icone);
+        
+        //posicionando os botoes
+        //coluna, linha, largura, altura
+        novoBtn.setBounds(70, 20, 100, 30);
+        abrirBtn.setBounds(70, 70, 100, 30);
+        botaoBtn.setBounds(70, 120, 100, 30);
+        
+        //adicionando os JLabel na tela
+        tela.add(novoBtn);
+        tela.add(abrirBtn);
+        tela.add(botaoBtn);
+
+        setSize(250,210); //atribuindo um tamanho para tela
+        setVisible(true); //janela visivel
+        setLocationRelativeTo(null); //janela ao centro
+        setResizable(false); //janela fixa
+
+    }
+    
+}
---
+++ 2-ano/ds/JFrame/Botao/JBotaoPOO/JBotaoPOO/src/main/java/com/etec/jbotaopoo/Main.java
@@ -0,0 +1,15 @@
+package com.etec.jbotaopoo;
+
+//@author Pedro
+
+//importação das bibliotecas
+import javax.swing.*; 
+import java.awt.*;
+
+
+public class Main extends JFrame{
+    public static void main(String[] args) {
+        ExemploBotao app = new ExemploBotao(); //instancia do objeto
+        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //opções da janela
+    }
+}
